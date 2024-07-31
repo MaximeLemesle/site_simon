968f2b6125041c5110c3c328ff447a6e3e1f6ada
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-07-31 23:32:27 +0200

    🚧 Start Transformation pages | 31/07

--- pages/transformation/index.vue
+++ pages/transformation/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-        <h1 class="h1 primary-500">Transformation Page</h1>
-    </div>
+    <main>
+        <TransformationHero />
+        <TransformationReviews />
+    </main>
 </template>
